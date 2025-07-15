d1fc77e3b0f8c6a821c8a69125653a436433c945
ahmadalli <ahmadalli@users.noreply.github.com>
2025-07-15 16:29:06 +0000

    deploy: 2d1eaa384b177b53ac83a8da9dbfbf6f892d6d13

--- assets/js/5e95c892.f964389a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkpublic_notes=self.webpackChunkpublic_notes||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>p});r(6540);var u=r(4164),c=r(1213),n=r(7559),t=r(2831),a=r(7907),l=r(4848);function p(e){return(0,l.jsx)(c.e3,{className:(0,u.A)(n.G.wrapper.docsPages),children:(0,l.jsx)(a.A,{children:(0,t.v)(e.route.routes)})})}}}]);
